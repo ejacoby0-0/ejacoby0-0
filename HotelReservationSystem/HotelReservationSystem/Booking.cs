a9f43156d0ffa6afdaaa9321b361984aea992bee
ejacoby0-0 <ejacoby2099@gmail.com>
2025-10-17 09:08:27 +0200

    Add files via upload
    This is my first C# project for a hotel reservation system. Its requirements were:
    
    1. Prompting users to reserve rooms for specific dates
    2. Show available rooms based on specified check-in and check-out dates
    3. Manage guest check-ins when they arrive
    4. Handle the check-out process for guests
    5. View the reservation history for guests

---
+++ HotelReservationSystem/HotelReservationSystem/Booking.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationSystem
+{
+    class Booking
+    {
+        public string GuestName { get; set; }
+        public string RoomType { get; set; }
+        public DateTime CheckInDate { get; set; }
+        public DateTime CheckOutDate { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name: {GuestName}, Room: {RoomType}, Check-In: {CheckInDate.ToShortDateString()}, Check-Out: {CheckOutDate.ToShortDateString()}";
+        }
+    }
+}
